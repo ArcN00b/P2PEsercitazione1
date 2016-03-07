73f954d1cedc08d562163cbe01e997b153ec9e7f
Simone <armari.simone@gmail.com>
2016-03-07 22:35:44 +0100

    Implementato parser senza regex dato che i messaggi sono a lunghezza fissa, merge del master in branch:Simone

--- Parser.py
+++ Parser.py
@@ -1,27 +1,40 @@
-import re
+# Metodo statico che si occupa di suddividere i vari campi di data in modo consono
+def parse(data):
 
-class Parser:
+    # Inizializzo il contenitore dei vari campi
+    fields = None
 
-    # Inizializzo la stringa che contiene il comando e il vettore di stringhe che contengono i campi del messaggio
-    commands = None
-    fields = [None] * 3
+    # Prendo i primi 4 caratteri maiuscoli all'interno di data e li inserisco in command
+    command = data[0:4]
 
-    '''
-    Devo aggiungere le exception inerenti al fatto che p.search potrebbe restituire None
-    Per ora ci sono le regex che permettono il parsing del messaggio login
-    '''
+    # Se il comando è LOGI suddivido data in questo modo
+    if command == 'LOGI':
+        fields[0] = data[5:-6] #IPP2P[55B]
+        fields[1] = data[-5:] #PP2P[5B]
 
-    # Metodo che fa il parsing dei dati
-    def parse(self, data):
+    # Se il comando è ADDF o DELF suddivido data in questo modo
+    elif command == 'ADDF':
+        fields[0] = data[5:21] #SessionID[16]
+        fields[1] = data[22:38] #FileMD5[16]
+        fields[2] = data[-100:] #Filename[16]
 
-        # Prendo i primi 4 caratteri maiuscoli all'interno di data e li inserisco in command
-        p = re.compile('^[A-Z]{4}')
-        self.command = p.search(data).group()
+    # Se il comando è DELF suddivido data in questo modo
+    elif command == 'DELF':
+        fields[0] = data[5:21] #SessionID[16]
+        fields[1] = data[-16:] #FileMD5[16]
 
-        # Trovo se è presente la componente IPP2P
-        p = re.compile('(\d{3}\.){3}\d{3}\|([0-9a-fA-F]{4}\:){7}[0-9a-fA-F]{4}')
-        self.fields[0] = p.search(data).group()
+    # Se il comando è FIND suddivido data in questo modo
+    elif command == 'FIND':
+        fields[0] = data[5:21] #SessionID[16]
+        fields[1] = data[-20:] #Ricerca[20]
 
-        # Trovo se è presente la componente PP2P e lo salvo il fields
-        p = re.compile('\d{5}$')
-        self.fields[1] = p.search(data).group()
+    # Se il comando è LOGO suddivido data in questo modo
+    elif command == 'LOGO':
+        fields[0] = data[-16:] #SessionID[16]
+
+    # Se questo else viene eseguito significa che il comando ricevuto non è previsto
+    else:
+        print('Errore durante il parsing del messaggio\n')
+
+    # Eseguo il return del comando e dei campi del messaggio
+    return command, fields
