75761ef49fa72cfc5443a7b8e459918559b58ace
marco <marco.tex@live.com>
2016-03-08 18:38:03 +0100

    creazione database da passare al worker da utilizzare nelle Response completamento del worker che accetta il protocollo e genera le risposte

--- MultiServer.py
+++ MultiServer.py
@@ -1,25 +1,41 @@
 import select
 import socket
-import Worker
+from Worker import *
+from ManageDB import *
 
 # Insieme di costanti utilizzate nel progetto
-TCP_IP4 = ''  # Con questo ip il bind viene effettuato su tutte le interfacce di rete
-TCP_IP6 = ''  # Con questo ip il bind viene effettuato su tutte le interfacce di rete
+TCP_IP4 = '127.0.0.1'  # Con questo ip il bind viene effettuato su tutte le interfacce di rete
+TCP_IP6 = '::1'  # Con questo ip il bind viene effettuato su tutte le interfacce di rete
 TCP_PORT = 3000
 
 class MultiServer:
 
+    database = None;
+
+    def __init__(self):
+        self.database = ManageDB()
+
     def start(self):
 
-        # Creo il socket ipv4, imposto l'eventuale riutilizzo, lo assegno all'ip e alla porta
-        server_socket4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket4.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket4.bind((TCP_IP4, TCP_PORT)) #forse bisogna gestire errori
+        # Creo il socket ipv4, imposto l'eventuale riutilizzo, lo assegno all'ip e alla
+        try:
+            server_socket4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_socket4.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket4.bind((TCP_IP4, TCP_PORT))
 
-        # Creo il socket ipv4, imposto l'eventuale riutilizzo, lo assegno all'ip e alla porta
-        server_socket6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-        server_socket6.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server_socket6.bind((TCP_IP6, TCP_PORT)) #forse bisogna gestire errori
+        # Gestisco l'eventuale exception
+        except socket.error as msg:
+            print('Errore durante la creazione del socket IPv4: ' + msg[1] + '\n')
+
+        # Creo il socket ipv6, imposto l'eventuale riutilizzo, lo assegno all'ip e alla porta
+        try:
+            server_socket6 = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+            server_socket6.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket6.bind((TCP_IP6, TCP_PORT))
+
+        # Gestisco l'eventuale exception
+        except socket.error as msg:
+            print('Errore durante la creazione del socket IPv4: ' + msg[1] + '\n')
 
         # Metto il server in ascolto per eventuali richieste sui socket appena creati
         server_socket4.listen(5)
@@ -40,15 +56,15 @@ class MultiServer:
             for s in input_ready:
 
                 # Il client si è collegato tramite socket IPv4, accetto quindi la sua richiesta avviando il worker
-                if s == client_socket4:
+                if s == server_socket4:
                     client_socket4, address4 = server_socket4.accept()
-                    client_thread = Worker(client_socket4)
+                    client_thread = Worker(client_socket4, self.database)
                     client_thread.run()
 
                 # Il client si è collegato tramite socket IPv6, accetto quindi la sua richiesta avviando il worker
-                elif s == client_socket6:
+                elif s == server_socket6:
                     client_socket6, address6 = server_socket6.accept()
-                    client_thread = Worker(client_socket6)
+                    client_thread = Worker(client_socket6, self.database)
                     client_thread.run()
 
         # Chiudo i socket
@@ -57,3 +73,6 @@ class MultiServer:
 
 tcpServer = MultiServer()
 tcpServer.start()
+
+print("ciao")
+print("pippo")
